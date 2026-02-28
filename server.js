e197eff5008e2a5bebd5f3f6ee5766b2387c907b
lancealfuerto18-cyber <lancealfuerto18@gmail.com>
2026-02-28 17:46:24 +0800

    fix render google auth

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const { google } = require("googleapis");
 
 async function readSheet() {
 
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: ["https://www.googleapis.com/auth/spreadsheets"]
-    });
+    const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
+const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+});
 
     const client = await auth.getClient();
 
